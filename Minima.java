2979d0eca065972bb963c0b4259b45a887fcf22a
TLmember01 <83204429+TLmember01@users.noreply.github.com>
2021-04-26 09:55:10 +0300

    Add files via upload

---
+++ Minima.java
@@ -0,0 +1,39 @@
+import java.util.Date;
+class Minima
+
+{
+    
+ private int kodikos;
+ 
+ private String username_apostolea;
+ 
+ private String username_paralipti;
+ 
+ private String titlos;
+ 
+ private String periexomeno;
+ 
+ private Date imerominia_ora;
+ 
+ public Minima(int kodikos, String username_apostolea, String username_paralipti, String titlos, String periexomeno, Date imerominia_ora)
+ 
+ {
+     this.kodikos = kodikos;
+     
+     this.username_apostolea  = username_apostolea;
+     
+     this.username_paralipti = username_paralipti;
+     
+     this.titlos = titlos;
+     
+     this.periexomeno = periexomeno;
+     
+     
+     
+    }
+    
+    
+    
+    
+    
+}
