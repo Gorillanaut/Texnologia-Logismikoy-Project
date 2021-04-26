aa7faed21e6e61b9d0e77bdf79d9a44fe56a52f3
jimvak <73341186+jimvak@users.noreply.github.com>
2021-04-26 10:58:38 +0300

    Add files via upload

---
+++ Aitisi_Pistopoihtikou.java
@@ -0,0 +1,30 @@
+import java.util.Date;
+
+
+class Aitisi_Pistopoihtikou
+{
+  private int kodikos_aitisis;
+  
+  private int kodikos;
+    
+  private String username_politi;
+  
+  private Date imerominia_kataxorisis;
+  
+  private String katastasi;
+  
+  public Aitisi_Pistopoihtikou(int kodikos_aitisis, int kodikos, Date imerominia_kataxorisis, String katastasi)
+  
+  {
+      this.kodikos_aitisis = kodikos_aitisis;
+      
+      this.kodikos = kodikos;
+      
+      this.imerominia_kataxorisis = imerominia_kataxorisis;
+      
+      this.katastasi = katastasi;
+      
+      
+    }
+  
+}
