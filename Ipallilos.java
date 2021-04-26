b270e859c38ab784de0794aa7f79d6b7fa900cd9
ABLAMMALBA <83204165+ABLAMMALBA@users.noreply.github.com>
2021-04-26 10:57:32 +0300

    Add files via upload

--- Ipallilos.java
+++ Ipallilos.java
@@ -19,6 +19,42 @@ class Ipallilos
      
     }
     
+ public String get_password()
+ 
+ {
+     return password;
+     
+    }
+ 
+ public void set_password(String password)
+ 
+ {
+     this.password = password;
+     
+    }
     
+ public String get_username()
+ 
+ {
+     return username;
+    }
     
+ public void set_username(String username)
+ 
+ {
+     this.username = username;
+    }
+ 
+ public String get_onomateponimo()
+ 
+ {
+     return onomateponimo;
+    }
+ 
+    
+ public void set_onomateponimo(String onomateponimo)
+ 
+ {
+     this.onomateponimo = onomateponimo;
+    }
 }
