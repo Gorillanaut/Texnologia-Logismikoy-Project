1bfc77e05375cd7cb0d082d7c060d6fab4070490
ABLAMMALBA <83204165+ABLAMMALBA@users.noreply.github.com>
2021-04-26 10:57:49 +0300

    Add files via upload

---
+++ Parapono.java
@@ -0,0 +1,41 @@
+import java.util.Date;
+
+class Parapono
+
+{
+ 
+    private int kodikos;
+    
+    private String titlos;
+    
+    private String perigrafi;
+    
+    private String username_politi;
+    
+    private Date imerominia_kataxorisis;
+    
+    private String path_eikonas;
+    
+    private String Sxolia;
+    
+    public Parapono(int kodikos, String titlos, String perigrafi, String username_politi, Date imerominia_kataxorisis, String path_eikonas, String sxolia)
+    
+    {
+        this.kodikos = kodikos;
+        
+        this.titlos = titlos;
+        
+        this.perigrafi = perigrafi;
+        
+        this.username_politi = username_politi;
+        
+        this.imerominia_kataxorisis = imerominia_kataxorisis;
+        
+        this.path_eikonas = path_eikonas;
+        
+        this.Sxolia = sxolia;
+        
+    }
+    
+    
+}
