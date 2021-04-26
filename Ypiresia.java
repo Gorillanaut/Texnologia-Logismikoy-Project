135c42bdac3a64a971537962e6e42beec1dcee60
Gorillanaut <80041563+Gorillanaut@users.noreply.github.com>
2021-04-26 11:00:37 +0300

    Add files via upload

---
+++ Ypiresia.java
@@ -0,0 +1,31 @@
+import java.util.Date;
+
+class Ypiresia
+
+
+{
+    
+ private int kodikos;
+ 
+ private String titlos;
+ 
+ private Date orario_leitourgias;
+ 
+ private Ipallilos ipefthinos_ypallilos;
+ 
+ public Ypiresia(int kodikos, String titlos, Date orario_leitourgias, Ipallilos ipefthinos_ypallilos)
+ 
+ {
+     this.kodikos = kodikos;
+     
+     this.titlos = titlos;
+     
+     this.orario_leitourgias = orario_leitourgias;
+     
+     this.ipefthinos_ypallilos = ipefthinos_ypallilos;
+     
+    }
+ 
+    
+    
+}
