a2ab88da3d26764048f27ae042eb78ee6b7734b2
TLmember01 <83204429+TLmember01@users.noreply.github.com>
2021-04-26 10:59:21 +0300

    Add files via upload

---
+++ Rantevou.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+class Rantevou
+
+{
+ 
+    private int kodikos;
+    
+    private Date imerominia_ora;
+    
+    private String username_politi;
+    
+    private int kodikos_ypiresias;
+    
+    public Rantevou(int kodikos, Date imerominia_ora, String username_politi, int kodikos_ypiresias)
+    
+    {
+       this.kodikos = kodikos;
+       
+       this.imerominia_ora = imerominia_ora;
+       
+       this.username_politi = username_politi;
+       
+       this.kodikos_ypiresias = kodikos_ypiresias;
+        
+    }
+    
+}
---
+++ Statistika.java
@@ -0,0 +1,36 @@
+import java.util.Date;
+
+class Statistika
+
+
+{
+    
+ private int kodikos;
+ 
+ private String username_politi;
+ 
+ private Date teleftaia_eisodos;
+ 
+ private int plithos_ekkremwn_aitisewn;
+ 
+ private int plithos_ekkremwn_paraponwn;
+ 
+ 
+ 
+ public Statistika(int kodikos, String username_politi, Date teleftaia_eisodos, int plithos_ekkremwn_aitisewn, int plithos_ekkremwn_paraponwn)
+ 
+ {
+     this.kodikos = kodikos;
+     
+     this.username_politi = username_politi;
+     
+     this.teleftaia_eisodos = teleftaia_eisodos;
+     
+     this.plithos_ekkremwn_aitisewn = plithos_ekkremwn_aitisewn;
+     
+     this.plithos_ekkremwn_paraponwn=plithos_ekkremwn_paraponwn;
+     
+    }
+ 
+    
+}
