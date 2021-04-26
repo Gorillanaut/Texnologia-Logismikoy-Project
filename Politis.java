32efc53bc230afe66a96478abdfaad5a254579ce
ABLAMMALBA <83204165+ABLAMMALBA@users.noreply.github.com>
2021-04-26 10:23:47 +0300

    Add files via upload

---
+++ Politis.java
@@ -0,0 +1,39 @@
+class Politis 
+
+
+{
+ 
+    private String onoma;
+    
+    private String eponimo;
+    
+    private String at;
+    
+    private String username;
+    
+    private String password;
+    
+    private String email;
+    
+    private String address;
+    
+    public Politis(String onoma, String eponimo, String at, String username, String password, String email, String address)
+    
+    {
+     this.onoma = onoma;
+     
+     this.eponimo = eponimo;
+     
+     this.at=at;
+     
+     this.username = username;
+     
+     this.password = password;
+     
+     this.email = email;
+     
+     this.address = address;
+        
+    }
+    
+}
