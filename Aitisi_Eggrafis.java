165bce15bdcc975031a9ebabb25663e1cfd04ec7
Gorillanaut <80041563+Gorillanaut@users.noreply.github.com>
2021-04-26 09:47:52 +0300

    Add files via upload

---
+++ Aitisi_Eggrafis.java
@@ -0,0 +1,53 @@
+import java.util.Date;
+class Aitisi_Eggrafis
+
+{
+ private String onoma;
+ 
+ private String eponimo;
+ 
+ private String at;
+ 
+ private String username;
+ 
+ private String password;
+ 
+ private String email;
+    
+ private String address;
+ 
+ private String path;
+    
+ private int kodikos_aitisis;
+ 
+ private Date imerominia_kataxorisis;
+ 
+ private String katastasi;
+ 
+ 
+ public Aitisi_Eggrafis(String onoma, String eponimo, String at, String username, String password, String email, String address, String path, Date imerominia_kataxorisis, String katastasi)
+ 
+ {
+     this.onoma = onoma;
+     
+     this.eponimo = eponimo;
+     
+     this.at = at;
+     
+     this.username = username;
+     
+     this.password = password;
+     
+     this.email = email;
+     
+     this.address=address;
+     
+     this.path = path;
+     
+     this.imerominia_kataxorisis =imerominia_kataxorisis;
+     
+     this.katastasi = katastasi;
+     
+    }
+ 
+}
