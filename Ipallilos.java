23bd9202a0dc36d57be1c0a5df0cb47e0ea947a7
ABLAMMALBA <83204165+ABLAMMALBA@users.noreply.github.com>
2021-04-26 09:52:46 +0300

    Add files via upload

---
+++ Ipallilos.java
@@ -0,0 +1,24 @@
+class Ipallilos
+
+{
+ private String password;
+ 
+ private String username;
+ 
+ private String onomateponimo;
+ 
+ public Ipallilos(String password, String username, String onomateponimo)
+ 
+ {
+     this.password = password;
+     
+     this.username = username;
+     
+     this.onomateponimo = onomateponimo;
+     
+     
+    }
+    
+    
+    
+}
