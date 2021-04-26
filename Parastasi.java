d453367d3d574e3f335497d04b83d449389558ba
ABLAMMALBA <83204165+ABLAMMALBA@users.noreply.github.com>
2021-04-26 10:58:00 +0300

    Add files via upload

---
+++ Parastasi.java
@@ -0,0 +1,37 @@
+import java.util.Date;
+
+class Parastasi
+
+
+{
+   private int kodikos;
+   
+   private String titlos;
+   
+   private String perigrafi;
+   
+   private int theseis;
+   
+   private int eisitiria;
+   
+   private Date imerominia_ora;
+   
+   public Parastasi(int kodikos, String titlos, String perigrafi, int theseis, int eisitiria, Date imerominia_ora)
+   
+   {
+     this.kodikos = kodikos;
+     
+     this.titlos = titlos;
+     
+     this.perigrafi = perigrafi;
+     
+     this.theseis = theseis;
+     
+     this.eisitiria = eisitiria;
+     
+     this.imerominia_ora = imerominia_ora;
+       
+    }
+    
+    
+}
