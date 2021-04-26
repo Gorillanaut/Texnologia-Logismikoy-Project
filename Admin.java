70b5cefe85510991f8da9b1ec880cbc71198a1f9
Gorillanaut <80041563+Gorillanaut@users.noreply.github.com>
2021-04-26 10:22:55 +0300

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,19 @@
+class Admin
+
+{
+    
+ private String password;
+ 
+ private String username;
+ 
+ public Admin(String password, String username)
+ 
+ {
+     this.password = password;
+     
+     this.username = username;
+     
+    }
+    
+    
+}
