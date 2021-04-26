6c5856f70f3b8e262669b0d55819680ce919978b
Gorillanaut <80041563+Gorillanaut@users.noreply.github.com>
2021-04-26 11:00:30 +0300

    Add files via upload

---
+++ Thesi_Parking.java
@@ -0,0 +1,28 @@
+class Thesi_Parking
+
+{
+    
+ private int kodikos;
+ 
+ private String address;
+ 
+ private String katastasi;
+ 
+ private int diarkeia;
+ 
+ 
+ public Thesi_Parking(int kodikos, String address, String katastasi, int diarkeia)
+ 
+ {
+     this.kodikos = kodikos;
+     
+     this.address = address;
+     
+     this.katastasi = katastasi;
+     
+     this.diarkeia = diarkeia;
+    }
+ 
+ 
+    
+}
