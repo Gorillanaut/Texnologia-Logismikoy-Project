ac1e3c945b64df57f2e0821441d28193f7005afa
jimvak <73341186+jimvak@users.noreply.github.com>
2021-04-26 09:51:20 +0300

    Add files via upload

---
+++ Eisitirio.java
@@ -0,0 +1,42 @@
+class Eisitirio
+
+
+{
+    
+ private int kodikos;
+ 
+ private String typos;
+ 
+ private float timi;
+ 
+ // edo tha mpei antikeimeno anaforas 
+ private String antikeimeno;
+ 
+ private int kodikos_ekptosis;
+ 
+ private String stoixeia_katoxou;
+    
+    
+ public Eisitirio(int kodikos, String typos, float timi, String antikeimeno, int kodikos_ekptosis, String stoixeia_katoxou)
+ 
+ {
+     this.kodikos = kodikos;
+     
+     this.typos = typos;
+     
+     this.timi = timi;
+     
+     this.antikeimeno = antikeimeno;
+     
+     this.kodikos_ekptosis = kodikos_ekptosis;
+     
+     this.stoixeia_katoxou = stoixeia_katoxou;
+     
+     
+     
+     
+    }
+    
+    
+    
+}
