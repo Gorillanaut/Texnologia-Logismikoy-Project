08e867c8f461b5f0c7c26f169de0c986e0b4fec3
TLmember01 <83204429+TLmember01@users.noreply.github.com>
2021-04-26 10:24:35 +0300

    Add files via upload

---
+++ Psifoforia.java
@@ -0,0 +1,39 @@
+import java.util.Date;
+
+class Psifoforia
+
+
+{
+    private int kodikos;
+    
+    private Date imerominia_enarksis;
+    
+    private Date imerominia_liksis;
+    
+    private String titlos;
+    
+    private String erotima;
+    
+    private String epiloges;
+    
+    private String apotelesmata;
+    
+    
+    public Psifoforia(int kodikos, Date imerominia_enarksis,Date imerominia_liksis, String titlos, String erotima, String epiloges, String apotelesmata)
+    
+    {
+        this.kodikos=kodikos;
+        
+        this.imerominia_enarksis = imerominia_enarksis;
+        
+        this.imerominia_liksis = imerominia_liksis;
+        
+        this.titlos = titlos;
+        
+        this.erotima = erotima;
+        
+        this.apotelesmata = apotelesmata;
+        
+    }
+    
+}
