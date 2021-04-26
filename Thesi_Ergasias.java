1b894b30886644be6056b92bd9e24c34f03a2598
Gorillanaut <80041563+Gorillanaut@users.noreply.github.com>
2021-04-26 11:00:15 +0300

    Add files via upload

---
+++ Thesi_Ergasias.java
@@ -0,0 +1,31 @@
+class Thesi_Ergasias
+
+{
+ private int kodikos;
+ 
+ private String perigrafi_thesis;
+    
+ private String prosonta;
+ 
+ private String username_ipallilou;
+ 
+ public Thesi_Ergasias(int kodikos, String perigrafi_thesis, String prosonta, String username_ipallilou)   
+    
+    
+   {
+       
+     this.kodikos = kodikos;
+     
+     this.perigrafi_thesis = perigrafi_thesis;
+     
+     this.prosonta = prosonta;
+     
+     this.username_ipallilou=username_ipallilou;
+       
+    }
+    
+    
+    
+    
+    
+}
