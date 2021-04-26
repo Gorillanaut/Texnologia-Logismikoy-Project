f71783960fbb250aaa2ab9055a2e1652e73f89b3
jimvak <73341186+jimvak@users.noreply.github.com>
2021-04-26 10:58:15 +0300

    Add files via upload

---
+++ Pliromi.java
@@ -0,0 +1,42 @@
+import java.util.Date;
+
+class Pliromi
+
+
+{
+ private int kodikos;
+ 
+ private String arithmos_kartas;
+ 
+ private String onomateponimo;
+ 
+ private float poso_pliromis;
+ 
+ private String username_politi;
+ 
+ private int kodikos_eisitiriou;
+    
+ private Date imerominia_pliromis;
+ 
+ public Pliromi(int kodikos, String arithmos_kartas, String onomateponimo, float poso_pliromis, String username_politi, int kodikos_eisitiriou, Date imerominia_pliromis)
+ 
+ {
+     this.kodikos = kodikos;
+     
+     this.arithmos_kartas = arithmos_kartas;
+     
+     this.onomateponimo = onomateponimo;
+     
+     this.poso_pliromis = poso_pliromis;
+     
+     this.username_politi = username_politi;
+     
+     this.kodikos_eisitiriou = kodikos_eisitiriou;
+     
+     this.imerominia_pliromis = imerominia_pliromis;
+     
+     
+     
+     
+    }
+}
