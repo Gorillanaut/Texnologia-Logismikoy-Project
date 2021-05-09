60268977e7bf2755721615cf9e535023cc097745
ABLAMMALBA <83204165+ABLAMMALBA@users.noreply.github.com>
2021-05-09 17:52:02 +0300

    Add files via upload

--- Thesi_Parking.java
+++ Thesi_Parking.java
@@ -23,6 +23,52 @@ class Thesi_Parking
      this.diarkeia = diarkeia;
     }
  
+  public int get_kodikos()
+  
+  {
+     return kodikos; 
+    }
  
+ public void set_kodikos(int k)
+ 
+ {
+     kodikos = k;
+    }
+    
+    public String get_address()
+ 
+ {
+     return address;
+    }
+ 
+ public void set_address(String a)
+ 
+ {
+     address=a;
+    }
+    
+      public String get_katastasi()
+ 
+ {
+     return katastasi;
+    }
+ 
+ public void set_katastasi(String a)
+ 
+ {
+     katastasi=a;
+    }
+    
+   public int get_diarkeia()
+  
+  {
+     return diarkeia; 
+    }
+ 
+ public void set_diarkeia(int d)
+ 
+ {
+     diarkeia = d;
+    } 
     
 }
