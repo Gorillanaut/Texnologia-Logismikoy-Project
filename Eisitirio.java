b98e5b467c8a171763951e30c72836a0f4d937ca
TLmember01 <83204429+TLmember01@users.noreply.github.com>
2021-05-09 18:01:40 +0300

    Add files via upload

--- Eisitirio.java
+++ Eisitirio.java
@@ -15,8 +15,81 @@ class Eisitirio
  private int kodikos_ekptosis;
  
  private String stoixeia_katoxou;
+ 
+ public int get_kodikos()
+ 
+ {return kodikos;
+     
+    }
     
+ public void set_kodikos(int k)
+ 
+ {
+     kodikos = k;
+    }
+ 
+ public String get_typos()
+ 
+ {
+     return typos;
+    }
+ 
+ public void set_typos(String t)
+ 
+ {
+     typos = t;
+    }
+ 
+ public float get_timi()
+ 
+ {
+     return timi;    
+    }
+ 
+ public void set_timi(float t)
+ 
+ {
+     timi = t;
+    }
+ 
+ public String get_antikeimeno()
+ 
+ { 
+     return antikeimeno;
+     
+    }
+ 
+ public void set_antikeimeno(String ant)
+ 
+ {
+     antikeimeno = ant;
+     
+    }
     
+ public int get_kodikos_ekptosis()
+ 
+ {
+     return kodikos_ekptosis;
+    }
+ 
+    
+ public void set_kodikos_ekptosis(int k)
+ 
+ {
+     kodikos_ekptosis = k;
+    }
+    
+ public String get_stoixeia_katoxou()
+ 
+ {
+     return stoixeia_katoxou;
+    }
+    
+ public void set_stoixeia_katoxou(String s)
+ 
+ {
+     stoixeia_katoxou=s;
+    }
  public Eisitirio(int kodikos, String typos, float timi, String antikeimeno, int kodikos_ekptosis, String stoixeia_katoxou)
  
  {
