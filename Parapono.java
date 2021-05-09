02bc7dec1de8ef4a306b019c51285541c98de8ac
jimvak <73341186+jimvak@users.noreply.github.com>
2021-05-09 17:56:48 +0300

    Add files via upload

--- Parapono.java
+++ Parapono.java
@@ -18,6 +18,91 @@ class Parapono
     
     private String Sxolia;
     
+    
+    
+    public int get_kodikos()
+    
+    {
+     return kodikos;   
+    }
+    
+    public void set_kodikos(int k)
+    
+    {
+        kodikos =k;
+    }
+    
+    public String get_titlos()
+    
+    {
+     return titlos;   
+    }
+    
+    public void set_titlos(String t)
+    
+    {
+     titlos = t;   
+    }
+    
+    public String get_perigrafi()
+    
+    {
+     return perigrafi;   
+    }
+    
+    public void set_perigrafi(String p)
+    
+    {
+     perigrafi = p;   
+    }
+    
+    public String get_username_politi()
+    
+    {
+     return username_politi;   
+    }
+    
+    public void set_username_politi(String u)
+    {
+     username_politi = u;   
+    }
+    
+    public Date get_imerominia_kataxorisis()
+    
+    {
+     return imerominia_kataxorisis;
+    }
+    
+    public void set_imerominia_kataxorisis(Date i)
+    
+    {
+     imerominia_kataxorisis = i;   
+    }
+    
+    public String get_path_eikonas()
+    
+    {
+     return path_eikonas;   
+    }
+    
+    public void set_email(String pa)
+    
+    {
+     path_eikonas = pa;   
+    }
+    
+    public String get_Sxolia()
+    
+    {
+     return Sxolia;   
+    }
+    
+    public void set_Sxolia(String s)
+    
+    {
+     Sxolia = s;   
+    }
+    
     public Parapono(int kodikos, String titlos, String perigrafi, String username_politi, Date imerominia_kataxorisis, String path_eikonas, String sxolia)
     
     {
@@ -36,6 +121,4 @@ class Parapono
         this.Sxolia = sxolia;
         
     }
-    
-    
 }
