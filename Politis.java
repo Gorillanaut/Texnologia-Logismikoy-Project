4f64cff19b3eaf52cbd9d85389df7b6e3eb570c7
Gorillanaut <80041563+Gorillanaut@users.noreply.github.com>
2021-05-13 11:38:43 +0300

    Add files via upload

--- Politis.java
+++ Politis.java
@@ -17,6 +17,89 @@ class Politis
     
     private String address;
     
+    public String get_onoma()
+    
+    {
+     return onoma;   
+    }
+    
+    public void set_onoma(String o)
+    
+    {
+        onoma =o;
+    }
+    
+    public String get_eponimo()
+    
+    {
+     return eponimo;   
+    }
+    
+    public void set_eponimo(String e)
+    
+    {
+     eponimo = e;   
+    }
+    
+    public String get_at()
+    
+    {
+     return at;   
+    }
+    
+    public void set_at(String a)
+    
+    {
+     at = a;   
+    }
+    
+    public String get_username()
+    
+    {
+     return username;   
+    }
+    
+    public void set_username(String u)
+    {
+     username = u;   
+    }
+    
+    public String get_password()
+    
+    {
+        return password;
+    }
+    
+    public void set_password(String p)
+    
+    {
+     password = p;   
+    }
+    
+    public String get_email()
+    
+    {
+     return email;   
+    }
+    
+    public void set_email(String e)
+    
+    {
+     email = e;   
+    }
+    
+    public String get_address()
+    
+    {
+     return address;   
+    }
+    
+    public void set_address(String a)
+    
+    {
+     address = a;   
+    }
+    
     public Politis(String onoma, String eponimo, String at, String username, String password, String email, String address)
     
     {
