86acbdafdadcee54098a063632ae8bab8246424b
Gorillanaut <80041563+Gorillanaut@users.noreply.github.com>
2021-05-13 11:38:17 +0300

    Add files via upload

--- Minima.java
+++ Minima.java
@@ -14,7 +14,76 @@ class Minima
  private String periexomeno;
  
  private Date imerominia_ora;
+
+  public int get_kodikos()
+  
+  {
+     return kodikos; 
+    }
  
+ public void set_kodikos(int k)
+ 
+ {
+     kodikos = k;
+    }
+ 
+ public String get_username_apostolea()
+ 
+ {
+     return username_apostolea;
+    }
+ 
+ public void set_username_apostolea(String a)
+ 
+ {
+     username_apostolea=a;
+    }
+ 
+    public String get_username_paralipti()
+    
+    {
+     return username_paralipti;   
+    }
+    
+   public void set_username_paralipti(String a)
+   {
+     username_paralipti=a;  
+    }
+    
+   public String get_titlos()
+   
+   {
+     return titlos;    
+    }
+   
+   public void set_titlos(String t)
+   {
+     titlos = t;  
+    }
+   
+    public String get_periexomeno()
+    
+    {
+     return periexomeno;   
+    }
+    
+    public void set_periexomeno(String p)
+    
+    {
+     periexomeno = p;   
+    }
+    
+    public Date get_imerominia_ora()
+    
+    {
+     return imerominia_ora;   
+    }
+    
+    public void set_imerominia_ora(Date i)
+    
+    {
+     imerominia_ora=i;   
+    }
  public Minima(int kodikos, String username_apostolea, String username_paralipti, String titlos, String periexomeno, Date imerominia_ora)
  
  {
